78916683afab579ce65c8d9f32c5c1f922d3c4f2
Danielle Byrne <DanielleByrne421@gmail.com>
2020-08-09 18:50:07 -0400

    created heroku app and added to server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const path = require ("path");
+
 
 const app = express();
 
@@ -10,12 +12,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("client/build"));
+app.use(express.static("client/build"));
+
 
 app.get("/api/config", (req, res) => {
   res.json({
     success: true,
   });
 });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+   });
+   app.get("/api/config", (req, res) => {
+    res.json({ success: true });
+   });
+   
 
 
 mongoose
