63c67e641dbc89be738b95ce89ea737a67044910
Danielle Byrne <DanielleByrne421@gmail.com>
2020-08-11 19:53:58 -0400

    trying to get api call to work

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,6 @@ import axios from "axios";
 export default {
   searchBook: function (query) {
     return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+    //not sure if I'm making the api call wrong? I get a response but the data is undefined
   }
 };
